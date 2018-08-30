a56402247446439271de87c2f5f9c459bff2705d
godleon <godleon@gmail.com>
2018-08-30 10:14:40 +0800

    tidy up codes

--- install-k8s.sh
+++ install-k8s.sh
@@ -12,22 +12,11 @@ git clone https://github.com/kubernetes-incubator/kubespray.git
 cd -
 pip install -r /srv/kubespray/requirements.txt
 
-# spawn VMs for k8s dedeployment
-# cd /ansible
-# ansible-playbook -i inventory site.destroy-vm.yml
-# ansible-playbook -i inventory site.spawn-vm.yml
 
 BASE_DIR="/srv/kubespray"
 rm -rf ${BASE_DIR}/inventory/mycluster
-#cp -rfp ${BASE_DIR}/inventory/sample ${BASE_DIR}/inventory/mycluster
 mkdir -p ${BASE_DIR}/inventory/mycluster
 cp -r kubespray/* ${BASE_DIR}/inventory/mycluster/
-# cp /ansible/kubespray/tasks/pre-requisite.yml ${BASE_DIR}/
 
 cd ${BASE_DIR}
-# ansible-playbook -i inventory/mycluster/hosts.ini pre-requisite.yml
 ansible-playbook -b -i inventory/mycluster/hosts.ini cluster.yml
-
-#cd /ansible
-#ansible-playbook -i inventory site.post-install.yml
-
--- start.sh
+++ start.sh
@@ -1,14 +1,11 @@
 #!/bib/bash
 
 # install docker
-# sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# sudo apt-get update
-# sudo apt-get install -y docker-ce
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
 
 docker run --rm -v $(pwd):/k8s-deploy ubuntu:xenial bash -c "cd /k8s-deploy; bash install-k8s.sh"
-
-#docker run --name kubespray -td -v $(pwd):/k8s-deploy ubuntu:xenial /bin/bash
-
 
